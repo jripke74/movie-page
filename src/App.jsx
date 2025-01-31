279beb48706d63d6bcf195006a8559df9ee55abb
Jeff Ripke <jripke74@gmail.com>
2025-01-31 12:15:49 -0800

    add title, cleared css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+const Card = ({title}) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+    </div>
+  )
+}
+
 const App = () => {
-	return (
-		<h2>Functional Arrow Component</h2>
-	)
+  return (
+    <div>
+      <h2>Functional Arrow Component</h2>
+      <Card title="Star Wars" />
+      <Card title="The Lion King"/>
+      <Card title="Avatar"/>
+    </div>
+  )
 }
 
 export default App;
