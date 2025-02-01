b8f9ebc8c921c157c9d58747e2b3912a8bf1cdf4
Jeff Ripke <jripke74@gmail.com>
2025-02-01 12:16:06 -0800

    add assets, css

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+          </header>
+          <p>Search</p>
+        </div>
+      </div>
+      <div><h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1></div>
+    </main>
+  )
+}
+
+export default App;
