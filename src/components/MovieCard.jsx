8bd041726e10592b823bd20df776e9aa015aef78
Jeff Ripke <jripke74@gmail.com>
2025-02-04 12:12:28 -0800

    add MovieCard.jsx

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,11 @@
+
+const MovieCard = ({ title, vote_average, poster_path, release_date, original_language }) => {
+  return (
+    <div>
+      <p className="text-white">{title}</p>
+    </div>
+
+  )
+}
+
+export default MovieCard;
